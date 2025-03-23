7465454b1ff64f8a3d89bd9192d5c85221c576ff
Ethan GONÇALVES <goncalves.ethan@gmail.com>
2025-03-23 16:52:56 +0100

    feat: Set up window startup parameters and properties to hide and close
    Signed-off-by: Ethan GONÇALVES <goncalves.ethan@gmail.com>

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Text;
+﻿using System.ComponentModel;
+using System.Security;
+using System.Security.Principal;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,5 +22,33 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        StartUpParams();
+    }
+    private void StartUpParams()
+    {
+        this.Title = "Gestion du parc";
+        this.Height = 500;
+        this.Width = 800;
+
+        this.Closing += OnClosing;
+        this.Hide();
+        this.WindowState = WindowState.Minimized;
+    }
+
+    private void ShowMainWindow(object sender, RoutedEventArgs e)
+    {
+        this.Show();
+        this.WindowState = WindowState.Normal;
+    }
+
+    private void OnClosing(object? sender, CancelEventArgs e)
+    {
+        e.Cancel = true;
+        this.Hide();
+    }
+
+    private void ExitApplication(object sender, EventArgs e)
+    {
+        Application.Current.Shutdown();
     }
 }
