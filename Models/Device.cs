a85e9011a5244736f582e5a6629d1814b7c2bac2
Ethan GONÇALVES <goncalves.ethan@gmail.com>
2025-03-23 18:40:57 +0100

    feat: Create Device model with attributes
    Signed-off-by: Ethan GONÇALVES <goncalves.ethan@gmail.com>

---
+++ Models/Device.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkManagerWPF.Models
+{
+    public class Device
+    {
+        public string Brand { get; set; }
+        public string Processor { get; set; }
+        public long RAM { get; set; }
+        public long Storage { get; set; }
+        public string MACAddress { get; set; }
+        public string IPAddress { get; set; }
+        public int ParkId { get; set; }
+        public int RoomId { get; set; }
+    }
+}
