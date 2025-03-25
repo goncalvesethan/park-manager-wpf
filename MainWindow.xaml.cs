1c6301df322ecb6d16cc98a0f1a458825077af65
Ethan GONÇALVES <goncalves.ethan@gmail.com>
2025-03-25 14:05:11 +0100

    feat: Create method to update device data on API on manual trigger
    Signed-off-by: Ethan GONÇALVES <goncalves.ethan@gmail.com>

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,6 +16,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Net.Http;
 using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Windows.Forms;
 
 namespace ParkManagerWPF;
 
@@ -26,10 +28,14 @@ public partial class MainWindow : Window
 {
     public Device Device { get; set; }
 
+    private readonly HttpClient _httpClient;
+
     public MainWindow()
     {
         InitializeComponent();
         StartUpParams();
+
+        _httpClient = new HttpClient();
 
         Device = DeviceInformations.GetDeviceInfo();
         this.DataContext = this;
@@ -59,6 +65,30 @@ public partial class MainWindow : Window
 
     private void ExitApplication(object sender, EventArgs e)
     {
-        Application.Current.Shutdown();
+        System.Windows.Application.Current.Shutdown();
+    }
+
+    private async Task SubmitDataAsync(object data)
+    {
+        string apiUrl = $"http://localhost:5296/api/devices/mac/{Device.MacAddress}";
+
+        string jsonData = JsonConvert.SerializeObject(data);
+        
+        var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+        try
+        {
+            HttpResponseMessage response = await _httpClient.PutAsync(apiUrl, content);
+            WindowsNotification.make(3000, "Gestion du parc", "Les informations sur le poste ont bien été remontées.");
+        }
+        catch (Exception ex)
+        {
+            System.Windows.MessageBox.Show($"Une erreur s'est produite : {ex.Message}");
+        }
+    }
+
+    private async void SynchronizeData(object sender, RoutedEventArgs e)
+    {
+        await SubmitDataAsync(Device);
     }
 }
