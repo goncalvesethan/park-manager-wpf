863fbbd9349e09455efc0020815ef1f1a35d0776
Ethan GONÇALVES <goncalves.ethan@gmail.com>
2025-03-25 14:02:56 +0100

    feat: Create asset to generate windwos notification with time, title and message
    Signed-off-by: Ethan GONÇALVES <goncalves.ethan@gmail.com>

---
+++ Assets/WindowsNotification.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ParkManagerWPF.Assets
+{
+    public class WindowsNotification
+    {
+        public static NotifyIcon make(int time, string title, string message)
+        {
+            NotifyIcon notify = new NotifyIcon();
+            notify.Icon = System.Drawing.SystemIcons.Information;
+            notify.Visible = true;
+            notify.ShowBalloonTip(2000, "Gestion du parc", "Les informations ont bien été remontées", ToolTipIcon.Info);
+            notify.Dispose();
+
+            return notify;
+        }
+    }
+}
