b544b0113ab6c5fc04d931be7557c5401be999df
Ethan GONÇALVES <goncalves.ethan@gmail.com>
2025-04-01 12:35:33 +0200

    feat: Add async function to handle device action
    Signed-off-by: Ethan GONÇALVES <goncalves.ethan@gmail.com>

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,6 +20,9 @@ using System.Diagnostics;
 using System.Windows.Forms;
 using System.Security.Cryptography.Pkcs;
 using System.Windows.Threading;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using Newtonsoft.Json.Bson;
 
 namespace ParkManagerWPF;
 
@@ -31,7 +34,8 @@ public partial class MainWindow : Window
     public Device Device { get; set; }
 
     private readonly HttpClient _httpClient;
-    private DispatcherTimer _timer;
+    private DispatcherTimer _syncTimer;
+    private DispatcherTimer _actionTimer;
 
     public MainWindow()
     {
@@ -45,6 +49,7 @@ public partial class MainWindow : Window
         Device = DeviceInformations.GetDeviceInfo();
         this.DataContext = this;
         SyncDataTask();
+        SyncActionTask();
     }
     private void StartUpParams()
     {
@@ -99,6 +104,43 @@ public partial class MainWindow : Window
         }
     }
 
+    private async Task CheckPendingActionAsync(Device device)
+    {
+        try
+        {
+            string url = $"http://localhost:5296/api/actions/mac/{device.MacAddress}";
+
+            var action = await _httpClient.GetFromJsonAsync<Models.Action>(url);
+
+            if (action != null)
+            {
+                WindowsNotification.make(3000, $"Action détectée : {action.Type}", ToolTipIcon.Info);
+
+                await _httpClient.PatchAsync(url, null);
+
+                switch (action.Type.ToLower())
+                {
+                    case "lock":
+                        ExecuteAction.Lock();
+                        break;
+                    case "reboot":
+                        ExecuteAction.Reboot();
+                        break;
+                    case "shutdown":
+                        ExecuteAction.Shutdown();
+                        break;
+                }
+            } else
+            {
+                Debug.WriteLine("Aucune action détecté pour ce poste");
+            }
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"Erreur lors de la vérification des actions : {ex.Message}");
+        }
+    }
+
     public void SetAuthButtonLabel()
     {
         if (AuthenticationManager.IsUserLoggedIn())
@@ -131,12 +173,22 @@ public partial class MainWindow : Window
 
     private void SyncDataTask()
     {
-        _timer = new DispatcherTimer();
-        _timer.Interval = TimeSpan.FromMinutes(5);
-        _timer.Tick += async (sender, e) => await SubmitDataAsync(Device);
-        _timer.Start();
+        _syncTimer = new DispatcherTimer();
+        _syncTimer.Interval = TimeSpan.FromMinutes(5);
+        _syncTimer.Tick += async (sender, e) => await SubmitDataAsync(Device);
+        _syncTimer.Start();
 
         _ = SubmitDataAsync(Device);
+    }
+
+    private void SyncActionTask()
+    {
+        _actionTimer = new DispatcherTimer();
+        _actionTimer.Interval = TimeSpan.FromMinutes(1);
+        _actionTimer.Tick += async (sender, e) => await CheckPendingActionAsync(Device);
+        _actionTimer.Start();
+
+        _ = CheckPendingActionAsync(Device);
     }
 
 }
